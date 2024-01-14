8b59605fb086b1c2778866d812932b6eba722ac9
YToh <83625612+Tonton21-afk@users.noreply.github.com>
2024-01-14 21:36:16 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3 text-gray-900">
+                <h1 class="text-3xl font-semibold mb-1">Hello, World!</h1>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
